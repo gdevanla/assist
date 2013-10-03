deb917a568c953da2d641c2c5d534f43ded7bc29
gdevanla <sed#lok>
2013-10-03 11:06:57 -0700

    Made printer variable a static variable

--- apps/StateCarver/assist/src/main/java/com/ser/assist/statecarver/core/MethodTracer.java
+++ apps/StateCarver/assist/src/main/java/com/ser/assist/statecarver/core/MethodTracer.java
@@ -7,32 +7,34 @@ import java.io.*;
 public class MethodTracer {
 
     public static int counter=0;
+    private static PrintWriter pw = null;
     static{
         try {
             //dirty way to create a new file
             new FileWriter(
                     StateCarverConfiguration.v().getMethodTraceFileName(), false);
             FileUtils.cleanDirectory(new File(StateCarverConfiguration.v().getTraceDestination()));
+            pw = new PrintWriter(new BufferedWriter(
+                    new FileWriter(
+                            StateCarverConfiguration.v().getMethodTraceFileName(), true)));
+
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
     }
 
     public static void writeMethodTrace(String methodName, String className, long methodCounter){
-       PrintWriter pw = null;
-        try {
-            System.out.println("writeMethodTrace called2" + methodName + ":" + className + ":" + methodCounter);
-            pw = new PrintWriter(new BufferedWriter(
-                    new FileWriter(
-                            StateCarverConfiguration.v().getMethodTraceFileName(), true)));
-            System.out.println("Got the printer:2");
+       //PrintWriter pw = null;
+       // try {
+            //System.out.println("writeMethodTrace called2" + methodName + ":" + className + ":" + methodCounter);
+            //System.out.println("Got the printer:2");
             pw.write("counter: " + methodCounter + ":" + className + ": " + methodName + "\n");
-            pw.close();
-            System.out.println("closed it:2");
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            System.err.println("Error in write Method Trace");
-            System.exit(-1);
-        }
+            //pw.close();
+         //   System.out.println("closed it:2");
+        //} catch (IOException e) {
+        //    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        //    System.err.println("Error in write Method Trace");
+        //    System.exit(-1);
+        //}
     }
 }
